da2dfa69cd5c1b1badaaef341451e97f8fbb59b2
scortix <simone3.corti@mail.polimi.it>
2021-11-02 15:57:26 +0100

    Added comments

--- parameters.m
+++ parameters.m
@@ -3,15 +3,16 @@ clear all
 clc
 
 %J = 1e4* [2 0 0; 0 2 0; 0 0 2.5];
-J = [0.09 0 0; 0 0.14 0; 0 0 0.07];
-e = 0.1;
-mu = 398600;
-par = [14000 e deg2rad(53) deg2rad(30) 0];
-nt0 = 0;
+J = [0.09 0 0; 0 0.14 0; 0 0 0.07]; % Inertia Matrix
+e = 0.1; %Orbit eccentricity
+mu = 398600; %Gravitational Constant
+par = [14000 e deg2rad(53) deg2rad(30) 0]; % Orbital parameters (semimajor axis, eccentricity, inclination...
+% Longitude of the ascending node, argument of periapsis)
+nt0 = 0; %Starting point
 
 
 %% baricentre position
-solar_par.xg = [0.03 0 0]'; % [m]
+solar_par.xg = [0.03 0 0]'; % [m] Center of mass
 
 %% main body geometry
 solar_par.Ab = [ 0.1*0.2 , 0.1*0.2 , 0.1*0.3 , 0.1*0.3 , 0.2*0.3 , 0.2*0.3];  % vector that contains main body surface [m^2]
@@ -51,6 +52,7 @@ n8 = -n7;
 n9 = [1; 0; 0];
 n10 = -n7;
 solar_par.ns = [ n7 n8 n9 n10 ]; 
-
+ 
+% Creation of bus to use in Simulink
 solar_par_info = Simulink.Bus.createObject(solar_par);
 solar_par_bus = evalin('base', solar_par_info.busName);
