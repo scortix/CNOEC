20468331492b9b1cd4d4c6229784bb65df73f274
mattewthep <matteo19982907@hotmail.it>
2022-08-08 10:42:44 +0200

    small adjustments

--- NMPC/MainNMPC.m
+++ NMPC/MainNMPC.m
@@ -78,39 +78,29 @@ csiRef = traj_csi(:,1:M);
 angleRef = traj_angle(:,1:M);
 yRef = traj_state(:,1:M);
 %%
-uref = zeros(3,M);
-for k = 1:M
-    uref(:,k) = csiRef(k)*[sin(angleRef(k)); cos(angleRef(k)); 0];
-end
 Q = eye(6);
-R = eye(3);
-E = eye(6);
+R = 1;
 
-x0 = zeros(15*M,1);
-
-%%
-for k = 1:M
-    x0(3*(k-1)+1:3*k) = uref(:,k);
-    x0(3*M+6*(k-1)+1:3*M+6*k) = yRef(:,k);
-end
+x0 = zeros(3*M,1);
+x0(1:M,1) = csiRef;
+x0(M+1:2*M,1) = angleRef;
 
 %%
 opt = myoptimset();
 mass = 1000;
 coder.extrinsic("opt_problem");
-xopt = opt_problem(x0,M,Ts,zEOE0,yRef,Q,R,E,coeffT,mass,opt);
+xopt = opt_problem(x0,M,Ts,zEOE0,yRef,Q,R,coeffT,mass,opt,Tmax);
 %%
-u = xopt(1:3,1);
-xi = norm(u)*1e3*mass/Tmax;
+xi = xopt(1);
+u = xi*[sin(xopt(M+1,1))*cos(xopt(2*M+1,1)) cos(xopt(M+1,1))*cos(xopt(2*M+1,1)) sin(xopt(2*M+1,1))]'*Tmax/mass/1e3;
 
 
-function xopt = opt_problem(x0,M,Ts,zEOE0,yRef,Q,R,E,coeffT,mass,opt)
-fun = @(x) NMPCcost(M,Ts,x,zEOE0,yRef,Q,R,E,coeffT,mass);
-xopt = mySQP(fun,x0,[],[],[],[],6*M,1+M,opt);
+function xopt = opt_problem(x0,M,Ts,zEOE0,yRef,Q,R,coeffT,mass,opt,Tmax)
+fun = @(x) NMPCcost(M,Ts,x,zEOE0,yRef,Q,R,coeffT,mass,Tmax);
+xopt = mySQP(fun,x0,[],[],[],[],0,3,opt);
 end
 
-
-function J = NMPCcost(M,Ts,x,y0,yref,Q,R,E,coeffT,m0)
+function J = NMPCcost(M,Ts,x,y0,yref,Q,R,coeffT,m0,Tmax)
 
 %COST Function calculates the cost of the orbital maneuvre considering
 %   arbitrary defined weigthed sums. The cost takes into account both the
@@ -123,41 +113,41 @@ function J = NMPCcost(M,Ts,x,y0,yref,Q,R,E,coeffT,m0)
 %       u0: input vector from trajectory
 %       ybar: desired state vector
 %       umax: maximum value for inputs
-%       x: optimization variables 3*M inputs,  6*M states (current state given), 6*M slack
+%       x: optimization variables 3*M inputs,  6*M states (current state given)
 %       variables in this order
 %       Q: state trajectory weights 6x6
-%       R: input trajectory weights 3x3
-%       E: slack variables weights  6x6 (xi for slack variables)
+%       R: fuel consumption weight
 
 % t = 0:Ts:Ts*M; % Time vector
 
 yhat = zeros(6,M+1); % Initialization of state vectors
 yhat(:,1) = y0; % Set initial conditions
 u = zeros(3,M); % Initialization of input vectors
-xi = zeros(6,M); % Initialization of slack variables
-g = zeros(6*M,1); % Initialization of equality constraints vector
-hu = zeros(1,M); % Initialization of fuel consumption constraint
+xi = zeros(1,M); % Initialization of fuel utilization
+qAngle = zeros(1,M); % Initialization of qAngle
+rAngle = zeros(1,M); % Initialization of rAngle
 m = zeros(1,M); m(1) = m0; % Initialization of mass
 
 for k = 1:M
-    u(:,k) = x(3*(k-1)+1:3*k,1);
-    yhat(:,k+1) = x(3*M+6*(k-1)+1:3*M+6*k,1);
-    xi(:,k) = x(9*M+6*(k-1)+1:9*M+6*k,1);
+    xi(1,k) = x(k,1);
+    qAngle(1,k) = x(M+k,1);
+    rAngle(1,k) = x(2*M+k,1);
 end
 
-F = zeros(15*M,1); 
+F = zeros(7*M,1);
 % Cost Function Calculation
 for k = 1:M
-    F(3*(k-1)+1:3*k,1) = sqrt(R)*u(:,k);
-    F(3*M+6*(k-1)+1:3*M+6*k,1) = sqrt(Q)*(yref(:,k)-yhat(:,k+1));
-    F(9*M+6*(k-1)+1:9*M+6*k,1) = sqrt(E)*xi(:,k);
-    g(6*(k-1)+1:6*k,1) = yhat(:,k)+Ts*EOEDerivatives(0,yhat(:,k),u(:,k),398600)-yhat(:,k+1)+xi(:,k);
-    hu(1,k) = sqrt(u(1,k)^2+u(2,k)^2+u(3,k)^2)*1e3*m(k);
+    u(:,k) = Tmax/m(k)/1e3*xi(1,k)*[sin(qAngle(k))*cos(rAngle(k)) cos(qAngle(k))*cos(rAngle(k)) sin(rAngle(k))]';
+    yhat(:,k+1) = yhat(:,k)+Ts*EOEDerivatives(0,yhat(:,k),u(:,k),398600);
+    F(k,1) = sqrt(R)*xi(1,k);
+    F(M+6*(k-1)+1:M+6*k,1) = sqrt(Q)*(yref(:,k)-yhat(:,k+1));
     if k < M
-        m(k+1) = m(k) - Ts*hu(k)/m(k)/1e3;
+        m(k+1) = m(k) - Ts*xi(1,k)*coeffT;
     end
 end
 
 r = min((yhat(1,:)./(1+sqrt(yhat(2,:).^2+yhat(3,:).^2).*cos(yhat(6,:)-atan2(yhat(3,:),yhat(2,:)))))');
-J = [F'*F;g;[r-6378.1 coeffT-hu]';F];
+minXi = min(xi);
+maxXi = max(xi);
+J = [F'*F;[r-6378.1 1-maxXi minXi]';F];
 end
--- NMPC/NMPC_cost.m
+++ NMPC/NMPC_cost.m
@@ -1,4 +1,4 @@
-function J = NMPC_cost(M,Ts,x,y0,yref,Q,R,E,coeffT,m0)
+function J = NMPC_cost(M,Ts,x,y0,yref,Q,R,coeffT,m0,Tmax)
 
 %COST Function calculates the cost of the orbital maneuvre considering
 %   arbitrary defined weigthed sums. The cost takes into account both the
@@ -11,41 +11,41 @@ function J = NMPC_cost(M,Ts,x,y0,yref,Q,R,E,coeffT,m0)
 %       u0: input vector from trajectory
 %       ybar: desired state vector
 %       umax: maximum value for inputs
-%       x: optimization variables 3*M inputs,  6*M states (current state given), 6*M slack
+%       x: optimization variables 3*M inputs,  6*M states (current state given)
 %       variables in this order
 %       Q: state trajectory weights 6x6
-%       R: input trajectory weights 3x3
-%       E: slack variables weights  6x6 (xi for slack variables)
+%       R: fuel consumption weight
 
 % t = 0:Ts:Ts*M; % Time vector
 
 yhat = zeros(6,M+1); % Initialization of state vectors
 yhat(:,1) = y0; % Set initial conditions
 u = zeros(3,M); % Initialization of input vectors
-xi = zeros(6,M); % Initialization of slack variables
-g = zeros(6*M,1); % Initialization of equality constraints vector
-hu = zeros(1,M); % Initialization of fuel consumption constraint
+xi = zeros(1,M); % Initialization of fuel utilization
+qAngle = zeros(1,M); % Initialization of qAngle
+rAngle = zeros(1,M); % Initialization of rAngle
 m = zeros(1,M); m(1) = m0; % Initialization of mass
 
 for k = 1:M
-    u(:,k) = x(3*(k-1)+1:3*k);
-    yhat(:,k+1) = x(3*M+6*(k-1)+1:3*M+6*k);
-    xi(:,k) = x(9*M+6*(k-1)+1:9*M+6*k);
+    xi(1,k) = x(k,1);
+    qAngle(1,k) = x(M+k,1);
+    rAngle(1,k) = x(2*M+k,1);
 end
 
-F = zeros(15*M,1); 
-%% Cost Function Calculation
+F = zeros(7*M,1);
+% Cost Function Calculation
 for k = 1:M
-    F(3*(k-1)+1:3*k) = sqrt(R)*u(:,k);
-    F(3*M+6*(M-1)+1:3*M+6*k) = sqrt(Q)*(yref(:,k)-yhat(:,k+1));
-    F(9*M+6*(M-1)+1:9*M+6*k) = sqrt(E)*xi(:,k);
-    g(6*(k-1)+1:6*k) = yhat(:,k)+Ts*EOEDerivatives(0,yhat(:,k),u(:,k),398600)-yhat(:,k+1)+xi(:,k);
-    hu(k) = sqrt(u(1,k)^2+u(2,k)^2+u(3,k)^2)*1e3*m(k);
+    u(:,k) = Tmax/m(k)/1e3*xi(1,k)*[sin(qAngle(k))*cos(rAngle(k)) cos(qAngle(k))*cos(rAngle(k)) sin(rAngle(k))]';
+    yhat(:,k+1) = yhat(:,k)+Ts*EOEDerivatives(0,yhat(:,k),u(:,k),398600);
+    F(k,1) = sqrt(R)*xi(1,k);
+    F(M+6*(k-1)+1:M+6*k,1) = sqrt(Q)*(yref(:,k)-yhat(:,k+1));
     if k < M
-        m(k+1) = m(k) - Ts*hu(k)/m(k)/1e3;
+        m(k+1) = m(k) - Ts*xi(1,k)*coeffT;
     end
 end
 
 r = min((yhat(1,:)./(1+sqrt(yhat(2,:).^2+yhat(3,:).^2).*cos(yhat(6,:)-atan2(yhat(3,:),yhat(2,:)))))');
-J = [F'*F;g;[r-6378.1 coeffT-hu]';F];
+minXi = min(xi);
+maxXi = max(xi);
+J = [F'*F;[r-6378.1 1-maxXi minXi]';F];
 end
